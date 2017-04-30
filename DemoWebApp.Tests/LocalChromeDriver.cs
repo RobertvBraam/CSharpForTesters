b53738c8cef1c8d9658e5b61440de1168d802a94
Robert van Braam <rhvbraamh@live.nl>
2017-04-30 22:18:15 +0200

    Initial commit of the demo solution

---
+++ DemoWebApp.Tests/LocalChromeDriver.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWebApp.Tests
+{
+    public class LocalChromeDriver : ChromeDriver
+    {
+        public LocalChromeDriver() : base(@"C:\Users\Robert\Desktop")
+        {
+
+        }
+    }
+}
