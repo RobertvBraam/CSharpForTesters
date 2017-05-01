3bf1fbaf9417df9a5306e09d7eee058864e72075
Robert van Braam <r.v.braam-houckgeest@rdc.nl>
2017-05-01 14:01:14 +0200

    Implemented a project with multiple cases for the presentation next to the selenium project and changed the presentation

---
+++ LINQ/Test/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(nameof(TellMeLess));
+            ITalker talker = new TellMeLess("Zo gaat het?");
+            talker.Talk("hoi");
+            Console.WriteLine();
+
+            Console.WriteLine(nameof(TellMeMore));
+            talker = new TellMeMore("hoe gaat het?");
+            talker.Talk("misschien");
+            Console.WriteLine();
+
+            Console.WriteLine(nameof(TellMeMore));
+            var newTalker = new TellMeMore("Daar Gaan We!");
+            newTalker.ExplicitTalk();
+            newTalker.Talk("doei");
+            Console.WriteLine();
+
+            Console.WriteLine(nameof(TellMeLess));
+            var oldTalker = new TellMeLess("Daar Gaan We DAN!");
+            oldTalker.ExplicitTalk();
+            oldTalker.Talk("Daag");
+
+            Console.ReadKey();
+        }
+    }
+
+    public class TellMeMore : ExplicitTalker, ITalker
+    {
+        public TellMeMore(string message)
+        {
+            base.Message = message;
+        }
+
+        public void Talk(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine(base.Message);
+        }
+    }
+
+    public sealed class TellMeLess : ExplicitTalker, ITalker
+    {
+        public TellMeLess(string message)
+        {
+            base.Message = message;
+        }
+
+        public void Talk(string message = "")
+        {
+            Console.WriteLine("...");
+            Console.WriteLine(base.Message);
+        }
+
+        public override void ExplicitTalk()
+        {
+            base.Message = base.Message.ToLower();
+        }
+    }
+
+    public interface ITalker
+    {
+        void Talk(string message);
+    }
+
+    public class ExplicitTalker
+    {
+        public string Message { get; set; }
+
+        public virtual void ExplicitTalk()
+        {
+            Message = Message.ToUpper();
+        }
+    }
+}
---
+++ TestProject/DemoWebApp.Tests/LocalChromeDriver.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWebApp.Tests
+{
+    public class LocalChromeDriver : ChromeDriver
+    {
+        private static ChromeOptions options {
+            get
+            {
+                ChromeOptions options = new ChromeOptions();
+                options.AddArgument("test-type");
+                options.AddArgument("start-maximized");
+                options.AddArgument("--js-flags=--expose-gc");
+                options.AddArgument("--enable-precise-memory-info");
+                options.AddArgument("--disable-popup-blocking");
+                options.AddArgument("--disable-default-apps");
+                options.AddArgument("test-type=browser");
+                options.AddArgument("disable-infobars");
+                return options;
+            }
+        }
+
+        public LocalChromeDriver() : base(@"C:\Users\RobertvBraam\Desktop", options)
+        {
+        }
+    }
+}
